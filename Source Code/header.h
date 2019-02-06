3266139e83ebd50b7778d587e839c5b6c5b81f36
Denver Edwards <denver.edwards14@gmail.com>
2019-02-06 14:02:18 -0500

    Moved functions into their appropriate struct

--- Source Code/header.h
+++ Source Code/header.h
@@ -7,6 +7,84 @@
 #pragma execution_character_set("utf-8")
 #pragma once
 
+void PrintIntro();
+char showMenu();
+
+// Algebra Function Prototypes
+struct Algebra {
+void additionFunc();
+void subtractionFunc();
+void multiplicationFunc();
+void divisionFunc();
+}
+
+// Trig Function Prototypes
+struct Trigonometry {
+void angleFunc();
+void angCompute();
+void angConvert();
+void refFunc();
+void identityFunc();
+void trigratio();
+void trig_ratio_solve();
+void trig_inverse_ratio_solve();
+void pythagorean();
+void oddeven();
+void cofunction(); 
+void angsumdiff();
+void hdtangle();
+void productsum();
+void lawFunc();
+void polarFunc();
+}
+
+// Geometry Function Prototypes
+struct Geometry {
+void area2dFunc();
+void area3dFunc();
+void circleFunc();
+void transFunc();
+void seqFunc();
+void quadFunc();
+void coorFunc();
+void expFunc();
+void vectorFunc();
+void measureFunc();
+void triangle2d();
+void rect2d();
+void parallel2d();
+void trape2d();
+void circle2d();
+void ellipse2d();
+void rectprism3d();
+void cylinder3d();
+void pyramid3d();
+void cone3d();
+void sphere3d();
+void cube3d();
+void torus3d();
+void ellipsoid3d();
+void arcmeasure();
+void sectorarea();
+void circeq();
+
+void rotate();
+void translate();
+void reflect();
+void dilation();
+}
+
+// Calculus Function Prototypes
+struct Calculus {
+void derivFunc();
+void integralFunc();
+}
+
+int endFunc();
+void errorFunc();
+
+const double pi = 3.14159; //TODO cmath library has M_PI for pi HENCE delete and replace
+
 
 char showMenu()
 {
